a894eef8ea4c83a0bceeb89630e16dd53cd20fba
coji <coji@techtalk.jp>
2025-05-21 23:39:12 +0900

    init

---
+++ main.ts
@@ -0,0 +1,14 @@
+import {GoogleGenAI} from '@google/genai';
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+
+const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.0-flash-001',
+    contents: 'Why is the sky blue?',
+  });
+  console.log(response.text);
+}
+
+main();
